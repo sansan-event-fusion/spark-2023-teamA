24610be6680dc4e1e9a25e6049a40d210413f140
nagayamajun <85071324+nagayamajun@users.noreply.github.com>
2023-09-11 18:12:09 +0900

    レビュー修正

--- src/feature/owner/type/schema.ts
+++ src/feature/owner/type/schema.ts
@@ -1,13 +1,13 @@
-import { INVALID_EMAIL_FORMAT_MESSAGE, REQUIRE_FIELD } from "@/constants/messages";
+import { EIGHT_CHARACTERS_OR_MORE, INVALID_EMAIL_FORMAT_MESSAGE, REQUIRE_FIELD } from "@/constants/messages";
 import * as z from "zod";
 
 //SignUpのschema
 export const signUpInputSchema = z.object({
   email: z.string().email(INVALID_EMAIL_FORMAT_MESSAGE),
-  password: z.string().min(8, "8文字以上で入力してください"),
+  password: z.string().min(8, EIGHT_CHARACTERS_OR_MORE),
   first_name: z.string().min(1, REQUIRE_FIELD),
   last_name: z.string().min(1, REQUIRE_FIELD),
-  phone_number: z.string().min(1, REQUIRE_FIELD)
+  phone_number: z.string().min(1, REQUIRE_FIELD),
   // phone_number: z.string().regex(/^\d{11}$/, '「-」無しで携帯番号をお願いします。')
 });
 export type SignUpInput = z.infer<typeof signUpInputSchema>;
