8d09eebbe18df0810dc8a583a1ac06f71219c610
Shota Mizusaki <nrxg129@gmail.com>
2023-09-13 16:53:07 +0900

    Homeにユーザー・管理会社のPathを追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import { PlainLink } from '@/components/atoms/Link'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -9,6 +10,8 @@ export default function Home() {
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
       <p>Fill HOME</p>
+      <PlainLink path='http://localhost:3001/rentalHouse' innerText='泊まるお部屋をお探しの方はこちら'/>
+      <a href='http://localhost:3001/admin/rentalHouse'>大家・管理会社の方はこちら</a>    
     </main>
   )
 }
