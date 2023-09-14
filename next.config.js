7cceccb1cdc60b2a0b4c4ffec9d534c45ac07a4d
nagayamajun <85071324+nagayamajun@users.noreply.github.com>
2023-09-15 06:53:36 +0900

    mansionRoomは作成で着るけどOwner側で取得はできない

--- next.config.js
+++ next.config.js
@@ -1,6 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+  reactStrictMode: false,
+  images: {
+    domains: ['firebasestorage.googleapis.com'], 
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+      },
+      {
+        protocol: "http",
+        hostname: "lh3.googleusercontent.com",
+      },
+      {
+        protocol: "http",
+        hostname: 'firebasestorage.googleapis.com',
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+
